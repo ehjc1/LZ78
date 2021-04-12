20f596d2e80e2ab9f293fb54a3796781b4676308
ehjc1 <ehjc1@lab-rg07-31.cms.waikato.ac.nz>
2021-04-12 17:43:29 +1200

    LZencode

---
+++ LZencode.java
@@ -0,0 +1,17 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Dictionary;
+import java.util.Map;
+
+public class LZencode{
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static void encode() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+
+    }
+}
