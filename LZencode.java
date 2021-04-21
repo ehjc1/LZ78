46b03d82818b56f997d66c1a57bca0c5a79354e1
JayEx23 <eugenechew2012@gmail.com>
2021-04-22 10:24:50 +1200

    updating impoort files

--- LZencode.java
+++ LZencode.java
@@ -1,17 +1,27 @@
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.util.Dictionary;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class LZencode{
+public class LZencode {
 
     public static void main(String[] args) {
-        
+
     }
 
-    public static void encode() {
+    public static void encode() throws Exception {
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        
+
+    }
+}
+
+class Trie {
+    class Node {
 
     }
 }
