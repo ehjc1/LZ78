1147ce2be3e599debe28d4b34214a5620389b7a0
JayEx23 <eugenechew2012@gmail.com>
2021-05-03 11:55:17 +1200

    working on encoder and testing trie

--- LZencode.java
+++ LZencode.java
@@ -9,7 +9,8 @@ public class LZencode {
 
     public static void encode() throws Exception {
         Reader reader = new InputStreamReader(System.in);
-        Trie t = null;
+        Trie trie = new Trie();
+        int phraseCounter;
         try {
             // stores byte that is read in as an integer
             int i = reader.read();
@@ -17,6 +18,8 @@ public class LZencode {
             // while where are still data to be read
             while (i != -1) {
                 // writer.write((int)i);
+                if (i == 10) { // check if we have finish
+                }
                 System.out.println(i);
                 i = (int) reader.read(); // read the next byte
             }
