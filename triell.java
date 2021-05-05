992635eee9d72a26e6c4b210f4b81e2e2448c03e
Zachary Cui <zacharycui2009@gmail.com>
2021-05-06 10:02:04 +1200

    foundation for backup trie

---
+++ triell.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class triell {
+
+    // Linked list structure:
+    // 2 linked lists
+    // Such that each node has:
+    // Left child & a right sibling
+
+    int index = 0;
+    int mismatch = 0;
+    triell rSibling;
+    triell fChild;
+    triell parent;
+
+    public triell(int mismatch) {
+
+    }
+
+    public void add(int mismatchChar, int phraseNum) {
+        
+    }
+}
