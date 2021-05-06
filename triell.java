13dce2cce91179325cf465c2f6fe8ed32f112c90
ZC77 <zc84@lab-rg07-29.cms.waikato.ac.nz>
2021-05-06 12:53:45 +1200

    Added features to linked-list trie

--- triell.java
+++ triell.java
@@ -7,17 +7,50 @@ public class triell {
     // Such that each node has:
     // Left child & a right sibling
 
-    int index = 0;
+    int pNumber = 0;
     int mismatch = 0;
     triell rSibling;
     triell fChild;
     triell parent;
 
-    public triell(int mismatch) {
+    static int largestPhraseNumber; 
+
+    public triell() {
+        rSibling = null;
+        fChild = null;
+        parent = null;
 
     }
 
-    public void add(int mismatchChar, int phraseNum) {
+    public void add(int mismatchChar) {
         
+        try {
+            if (fChild == null) { // This is a given, the 1st thing we're adding 
+                triell first = new triell(); 
+                fChild = first;
+                first.parent = this;
+                first.mismatch = mismatchChar;
+            }
+            else  { 
+                triell next = fChild.rSibling; // Scanning 'horiziontally' for any matches
+                while (next.rSibling != null) { 
+                    if (mismatch == next.mismatch) {
+                        triell child = new triell();
+                        child.parent = this;
+                        child.mismatch = mismatchChar;
+                    } else {
+                        next = fChild.rSibling;
+                    }
+                }
+                triell add = new triell();
+                add.mismatch = mismatchChar;
+                next.rSibling = add;
+    
+
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
     }
 }
